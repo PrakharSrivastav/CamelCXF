3a42e7dcf830ad0d5bff62354b5d75c5dd9ffb46
Prakhar <prakhar@Prakhars-MacBook-Pro.local>
2021-11-06 14:24:37 +0100

    extra comments

--- src/main/java/com/ps/Application.java
+++ src/main/java/com/ps/Application.java
@@ -7,8 +7,14 @@ import org.apache.camel.spi.PropertiesComponent;
 public class Application {
     public static void main(String[] args) throws Exception {
         Main main = new Main();
+
+        // add property file
         main.setDefaultPropertyPlaceholderLocation("classpath:application.properties");
+
+        // register the required routes
         main.configure().addRoutesBuilder(BankRouteBuilder.class);
+
+        // run the application
         main.run(args);
     }
 }
